54caf5d3e93bb6e0782cc379286704fd188c06b1
Brendan Berejikian <bezoric1@gmail.com>
2019-02-20 11:25:02 -0500

    Create CircleMathFunctions.cs

---
+++ ConsoleApp1/CircleMathFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MathFunctionCircleArea
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Circle Radius: ");
+        double radius = double.Parse(Console.ReadLine());
+        double pi = Math.PI;
+        double area = pi * (radius * radius);
+        double circumference = 2 * pi * radius;
+
+        Console.WriteLine("Area = " + area);
+        Console.WriteLine("Circle Radius: ");
+        Console.WriteLine("Circumference = " + circumference);
+        Console.ReadKey();
+      
+    }
+}
+
